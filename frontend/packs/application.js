c1fd19387a1ce36260304c0ac50e297588320518
Leonardo Flores <contato@leonardocouy.com>
2018-01-11 22:46:13 -0200

    HABEMUS VUILLLLLLL

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -1,3 +1,12 @@
 import "./application.css";
+import Vue from "vue/dist/vue.esm";
+import App from './app.vue'
 
-document.body.insertAdjacentHTML("afterbegin", "Webpacker works!");
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.appendChild(document.createElement('app'))
+  const app = new Vue({
+    render: h => h(App)
+  }).$mount('app')
+
+  console.log(app)
+})
